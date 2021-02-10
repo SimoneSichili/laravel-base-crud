72cf8198ce66e4319a2653422bc6d73f4ae70708
Simone <simone.sichili@gmail.com>
2021-02-10 15:10:03 +0100

    Create database. Migrate table. Show list in page

---
+++ resources/views/beers.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset("css/app.css") }}">
+    <title>Laravel CRUD Beer</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Le mie birre</h1>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Marca</th>
+                    <th>Tipo</th>
+                    <th>Prezzo</th>
+                    <th>Cl</th>
+                    <th>Gradi</th>
+                    <th>Creato il</th>
+                    <th>Aggiornato il</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($beers as $beer)
+                    <tr>
+                        <td>{{ $beer->id }}</td>
+                        <td>{{ $beer->brand }}</td>
+                        <td>{{ $beer->type }}</td>
+                        <td>{{ $beer->price }}€</td>
+                        <td>{{ $beer->cl }}</td>
+                        <td>{{ $beer->degrees }}</td>
+                        <td>{{ $beer->created_at }}</td>
+                        <td>{{ $beer->updated_at }}</td>
+                        <td><a href="">MOSTRA</a></td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
